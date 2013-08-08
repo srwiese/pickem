dafe07b2e5f0d05556834b67782443c87819136c
Scott Wiese <srwiese81@gmail.com>
2013-08-08 03:06:08 +0000

    added names.json

--- web.js
+++ web.js
@@ -4,11 +4,14 @@ var htmlfile = "index.html";
 
 //var app = express.createServer(express.logger());
 var app = express();
-
+  
 app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
   response.send(html);
 });
+
+app.use(express.static('/home/ubuntu/pickem'));
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
